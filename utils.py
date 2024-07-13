be5663af7c7e7b0e5dbdab4cf8aeecce4fc0251c
sherard_dalaguit <sherard.dalaguit@gmail.com>
2024-07-13 14:13:25 -0700

    Added BaseItem abstract base class in utils.py
    This commit introduces an abstract base class called BaseItem in utils.py. It generalizes the properties and methods an item should have in the game. This abstraction will serve as a template for defining specific items, forcing them to implement its methods to ensure consistent behavior.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from typing import NamedTuple
+from typing import NamedTuple, Any
+import abc
 
 
 class Stats(NamedTuple):
@@ -57,3 +58,89 @@ class Stats(NamedTuple):
 		magic_power = {self.magic_power},
 		special_trigger_chance = {self.special_trigger_chance}
 		"""
+
+
+class BaseItem(abc.ABC):
+	"""
+	BaseItem is an abstract base class the represents a generic
+	item in a game. Specific items should inherit from this class
+	and customize the 'name', 'passive_name', and 'calculate_effective_stats'
+	methods to match their individual behaviors.
+
+	The class serves as a contract for what each item should include
+	"""
+
+	def __init__(self, base_item_stats: Stats, is_passive_active: bool) -> None:
+		"""
+		Initialize an instance of the BaseItem class
+
+		Args:
+			base_item_stats (Stats): The base statistics associated with the item.
+			is_passive_active (bool): The state indicating if the passive is active.
+		"""
+		self.base_item_stats = base_item_stats
+		self.is_passive_active = is_passive_active
+
+	@property
+	@abc.abstractmethod
+	def name(self) -> str:
+		"""
+		Abstract method representing a 'name' property.
+
+		This method is intended to be overridden in subclasses to
+		return the name of the item as a string.
+
+		Returns:
+			str: The name of the item
+		"""
+		pass
+
+	@property
+	@abc.abstractmethod
+	def passive_name(self) -> str:
+		"""
+		Abstract method representing a 'passive_name' property
+
+		This property is intended to be overridden in subclasses to
+		return the passive name of the item as a string.
+
+		Returns:
+			str: The passive name of the item.
+		"""
+		pass
+
+	@abc.abstractmethod
+	def calculate_effective_stats(self, base_character_stats: Stats) -> Stats:
+		"""
+		Abstract method to calculate effective statistics.
+
+		This method should be overridden by subclasses to provide
+		functionality that takes base character stats
+		and calculates some effective stats.
+
+		Args:
+			base_character_stats (Stats): The original statistics of a
+			character before applying the item effect.
+
+		Returns:
+			Stats: The calculated statistics after applying the item effect.
+		"""
+		pass
+
+	def __eq__(self, other: Any) -> bool:
+		"""
+		Overrides the default implementation of the equality operator.
+		Two objects of class BaseItem are considered equal if the 'name'
+		properties are the same.
+
+		Args:
+			other (Any): The other object that this BaseItem will be compared to.
+
+		Returns:
+			bool: True if the other object is an instance of BaseItem and its
+			name property is the same as this BaseItem's. False otherwise
+		"""
+
+		if isinstance(other, BaseItem):
+			return self.name == other.name
+		return False
